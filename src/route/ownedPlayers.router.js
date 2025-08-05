c8f15432372d2112fe3331bb33c8b1de902f0df8
gguti <ggutips94@gmail.com>
2025-08-05 15:39:15 +0900

    파일명 수정 squard -> ownedPlayers

--- src/route/ownedPlayers.router.js
+++ src/route/ownedPlayers.router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
-import {myPlayersList, myPlayer} from '../controllers/squard.controller.js';
+import {myPlayersList, myPlayer} from '../controllers/ownedPlayers.controller.js';
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.use(authMiddleware);
 
 //내 선수 조회
 router.get(myPlayersList);
+
+//내 선수 상세 조회
 router.get(myPlayer)
 
 
