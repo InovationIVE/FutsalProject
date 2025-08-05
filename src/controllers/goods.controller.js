f44a957d8ab3f3064d0db50a5a61b6c16625928f
Youjin980225 <top04022@gmail.com>
2025-08-05 20:59:17 +0900

    ✨ feat  상품삭제, 상품수정 API 구현

--- src/controllers/goods.controller.js
+++ src/controllers/goods.controller.js
@@ -1,5 +1,6 @@
 import { gamePrisma } from '../utils/prisma/index.js';
 
+//상품 등록 API controller
 export const registGoods = async (req, res) => {
   const { name, cashAmount } = req.body;
 
@@ -16,7 +17,7 @@ export const registGoods = async (req, res) => {
     const response = {
       id: newGoods.goodsId,
       name: newGoods.name,
-      cash_amount: newGoods.cashAmount.toString(),
+      cashAmount: newGoods.cashAmount.toString(),
     };
     res.status(201).json(response);
   } catch (error) {
@@ -26,5 +27,71 @@ export const registGoods = async (req, res) => {
       return res.status(409).json({ message: '상품명이 존재합니다.' });
     }
     res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+  }
+};
+
+//상품 삭제 API controller
+export const deleteGoods = async (req, res) => {
+  const goodsId = parseInt(req.params.goodsId, 10);
+
+  if (isNaN(goodsId)) {
+    return res.status(400).json({ message: '상품ID가 유효하지 않습니다.' });
+  }
+  try {
+    const isExistGoods = await gamePrisma.goods.findUnique({
+      where: { goodsId },
+    });
+    if (!isExistGoods) {
+      return res.status(404).json({ message: '상품을 찾을 수 없습니다.' });
+    }
+
+    await gamePrisma.goods.delete({
+      where: { goodsId },
+    });
+
+    res.status(200).json({ message: '상품이 삭제되었습니다.' });
+  } catch (error) {
+    console.error('상품 삭제 오류:', error);
+    res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+  }
+};
+
+//상품 수정 API controller
+export const updateGoods = async (req, res) => {
+  const goodsId = parseInt(req.params.id, 10);
+  const { name, cashAmount } = req.body;
+
+  if (isNaN(goodsId)) {
+    return res.status(400).json({ message: '상품ID가 유효하지 않습니다.' });
+  }
+  if (!name && !cashAmount) {
+    return res.status(400).json({ message: '수정할 name 또는 cashAmount 값을 입력해 주세요.' });
+  }
+  try {
+    const isExistGoods = await gamePrisma.goods.findUnique({
+      where: { goodsId },
+    });
+    if (!isExistGoods) {
+      return res.status(404).json({ message: '상품을 찾을 수 없습니다.' });
+    }
+    const updateGoods = await gamePrisma.goods.update({
+      where: { goodsId },
+      data: {
+        ...(name && { name }),
+        ...(cashAmount && { cashAmount }),
+      },
+    });
+    return res.status(200).json({
+      message: '상품 수정이 완료되었습니다.',
+      name: updateGoods.name,
+      cashAmount: updateGoods.cashAmount,
+    });
+  } catch (error) {
+    console.error('상품 수정 오류:', error);
+
+    if (error.code === 'P2002') {
+      return res.status(409).json({ message: '상품명이 존재합니다.' });
+    }
+    return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
   }
 };
--- src/route/goods.router.js
+++ src/route/goods.router.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { gamePrisma } from '../utils/prisma/index.js';
 import { verifyToken, isAdmin } from '../middleware/authMiddleware.js';
-import { registGoods } from '../controllers/goods.controller.js';
+import { registGoods, deleteGoods, updateGoods } from '../controllers/goods.controller.js';
 
 const router = express.Router();
 
-//상품 등록 라우터
+//상품 등록 API router
 router.post('/admin/goods', verifyToken, isAdmin, registGoods);
+
+//상품 삭제 API router
+router.delete('/admin/goods/:goodsId', verifyToken, isAdmin, deleteGoods);
+
+//상품 수정 API router
+router.patch('/admin/goods/:goodsId', verifyToken, isAdmin, updateGoods);
 
 export default router;
