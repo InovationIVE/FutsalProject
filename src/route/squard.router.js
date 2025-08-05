0ea63d4824f61053367df3931af1c90c265cd0b4
gguti <ggutips94@gmail.com>
2025-08-05 15:28:27 +0900

    ⚡improve: 내선수조회 라우터, 컨트롤러 네이밍 수정
    myPlayerCorrection -> myPlayerList

--- src/route/squard.router.js
+++ src/route/squard.router.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
-import {myPlayersCorrection} from '../controllers/squard.controller.js';
+import {myPlayersList} from '../controllers/squard.controller.js';
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
 //내 선수 조회
-router.get(myPlayersCorrection);
+router.get(myPlayersList);
 
 
 
