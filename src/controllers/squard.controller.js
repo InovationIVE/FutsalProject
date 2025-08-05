7065deda9cda6d52551ec46bc3fbb5a7f6b0a0c1
gguti <ggutips94@gmail.com>
2025-08-05 15:22:33 +0900

    ✨feat: 내 선수 조회 기능 추가
    OwnedPlayers를 조회한다.

--- src/controllers/squard.controller.js
+++ src/controllers/squard.controller.js
@@ -1,29 +1,29 @@
 import { prisma } from '../utils/prisma/index.js';
 
 
-//내 선수 조회
-// export const myPlayersCorrection = async(req, res) => {
-//    const { accountId } = req.params;
-//    if(!accountId){
-//         return res.status(401).json({message: "내 계정만 조회 할 수 있습니다."});
-//     }
+// 내 선수 조회
+export const myPlayersCorrection = async(req, res) => {
+   const { accountId } = req.params;
+   if(!accountId){
+        return res.status(401).json({message: "내 계정만 조회 할 수 있습니다."});
+    }
 
-//     const myPlayers = await prisma.ownedPalyers.findMany({
-//         selcet: {
-//             soccerPlayerId: true,
-//             profileImage: true,
-//             name: true,
-//             speed: true,
-//             attack: true,
-//             defence: true,
-//             rarity: true,
-//         },
-//           orderBy: {
-//       createdAt: 'desc', //최신순으로 정렬합니다.
-//     },
-//     });
+    const myPlayers = await prisma.ownedPalyers.findMany({
+        selcet: {
+            soccerPlayerId: true,
+            profileImage: true,
+            name: true,
+            speed: true,
+            attack: true,
+            defence: true,
+            rarity: true,
+        },
+          orderBy: {
+      createdAt: 'desc', //최신순으로 정렬합니다.
+    },
+    });
     
-//     return res.status(200).json({data:myPlayers});
+    return res.status(200).json({data:myPlayers});
    
 };
 
--- src/route/squard.router.js
+++ src/route/squard.router.js
@@ -4,10 +4,10 @@ import {myPlayersCorrection} from '../controllers/squard.controller.js';
 
 const router = express.Router();
 
-// router.use(authMiddleware);
+router.use(authMiddleware);
 
-// //내 선수 조회
-// router.get(myPlayersCorrection);
+//내 선수 조회
+router.get(myPlayersCorrection);
 
 
 
