4f87f1626dcc22326ca9a38a69cdc68e27d193ea
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-08-05 14:03:13 +0900

    📝 chore::"패키지 수정 "
        "start": "node src/app.js",
        "dev": "nodemon src/app.js",
    
    src/ 붙임

---
+++ src/route/gacha.router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { gamePrisma } from '../utils/prisma';
+const router = express.Router();
+
+router.get('/admin/gacha', async (req, res) => {
+  try {
+    const gachaData = req.body;
+    if (!gachaData) {
+      return res.status(400).json({ error: '가챠 데이터가 없습니다' });
+    }
+
+  } catch (error) {
+    console.error('Error fetching gacha data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
