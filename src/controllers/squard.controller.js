5ab4a5921d0a3c9a1042fa212aba4017aaa1a16e
gguti <ggutips94@gmail.com>
2025-08-05 15:34:41 +0900

    ✨feat: 내선수 상세조회 기능 추가
    ⚡improve : 내 선수 목록 조회 기능 데이터 간소화

--- src/controllers/squard.controller.js
+++ src/controllers/squard.controller.js
@@ -2,7 +2,7 @@ import { prisma } from '../utils/prisma/index.js';
 
 
 // 내 선수 조회
-export const myPlayersCorrection = async(req, res) => {
+export const myPlayersList = async(req, res) => {
    const { accountId } = req.params;
    if(!accountId){
         return res.status(401).json({message: "내 계정만 조회 할 수 있습니다."});
@@ -12,10 +12,6 @@ export const myPlayersCorrection = async(req, res) => {
         selcet: {
             soccerPlayerId: true,
             profileImage: true,
-            name: true,
-            speed: true,
-            attack: true,
-            defence: true,
             rarity: true,
         },
           orderBy: {
@@ -24,7 +20,28 @@ export const myPlayersCorrection = async(req, res) => {
     });
     
     return res.status(200).json({data:myPlayers});
-   
+};
+
+//보유선수 상세 조회
+
+export const myPlayer = async(req, res)=>{
+    const accountId = req.user;
+    const { ownedPlayersId } = req.params;
+
+    const myPlayer = await prisma.ownedPalyers.findFirst({
+        where: { ownedPlayersId : +ownedPlayersId},
+        select:{
+             soccerPlayerId: true,
+            profileImage: true,
+            rarity: true,
+             name: true,
+            speed: true,
+            attack: true,
+            defence: true,
+        },
+    });
+
+    return res.status(200).json({data: myPlayer });
 };
 
    
--- src/route/squard.router.js
+++ src/route/squard.router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
-import {myPlayersList} from '../controllers/squard.controller.js';
+import {myPlayersList, myPlayer} from '../controllers/squard.controller.js';
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.use(authMiddleware);
 
 //내 선수 조회
 router.get(myPlayersList);
-
+router.get(myPlayer)
 
 
 export default router;
