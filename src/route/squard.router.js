212d729b49e786b4dd40ec21fda849d9d266d963
gguti <ggutips94@gmail.com>
2025-08-05 15:21:12 +0900

    🎉add: squard.controllers.js / squard.router.js 생성

---
+++ src/route/squard.router.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
+import {myPlayersCorrection} from '../controllers/squard.controller.js';
+
+const router = express.Router();
+
+// router.use(authMiddleware);
+
+// //내 선수 조회
+// router.get(myPlayersCorrection);
+
+
+
+export default router;
