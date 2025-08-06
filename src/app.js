8c7aeb578fcff494b5b152ab52da05e3139bc244
Youjin980225 <top04022@gmail.com>
2025-08-06 13:16:19 +0900

    ✨ feat 상품 라우터 import, 연걸 추가

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 
 // 라우터 import
 import authRouter from './route/auth.router.js';
+import goodsRouter from './route/goods.router.js';
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.use(cookieParser());
 
 // API 라우터 연결
 app.use('/auth', authRouter);
-
+app.use('/goods', goodsRouter);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
