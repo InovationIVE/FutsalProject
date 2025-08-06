c5f0141ddcba933c13ab89715f23c9f28f74ba48
Youjin980225 <top04022@gmail.com>
2025-08-06 19:05:26 +0900

    merge main

--- src/app.js
+++ src/app.js
@@ -5,11 +5,7 @@ import ErrorHandlingMiddleware from './middleWares/error-handling.middleware.js'
 import logMiddleware from './middleWares/log.middleware.js';
 import ownedPlayersRouter from './routes/ownedPlayers.router.js';
 import authRouter from './routes/auth.router.js';
-
-
-// 라우터 import
-import authRouter from './route/auth.router.js';
-import goodsRouter from './route/goods.router.js';
+import goodsRouter from './routes/goods.router.js';
 
 dotenv.config();
 
@@ -20,17 +16,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(logMiddleware);
 
-
-app.use('/api', [ownedPlayersRouter]);
+app.use('/api/ownPlayers', [ownedPlayersRouter]);
 app.use('/auth', [authRouter]);
+app.use('/api/goods', [goodsRouter]);
 
 app.use(ErrorHandlingMiddleware);
-
-
-
-// API 라우터 연결
-app.use('/auth', authRouter);
-app.use('/goods', goodsRouter);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
--- src/routes/goods.router.js
+++ src/routes/goods.router.js
@@ -6,19 +6,20 @@ import {
   updateGoods,
   getGoods,
 } from '../controllers/goods.controller.js';
-import { verifyToken, isAdmin } from '../middleware/authMiddleware.js';
+import { authMiddleware, requireAdmin } from './auth.router.js';
 
 const router = express.Router();
 
 /**상품 등록 API router(관리자)**/
-router.post('/admin/goods', verifyToken, isAdmin, registGoods);
+router.post('/admin/goods', authMiddleware, requireAdmin, registGoods);
 
 /**상품 삭제 API router(관리자)**/
-router.delete('/admin/goods/:goodsId', verifyToken, isAdmin, deleteGoods);
+router.delete('/admin/goods/:goodsId', authMiddleware, requireAdmin, deleteGoods);
 
 /**상품 수정 API router(관리자)**/
-router.patch('/admin/goods/:goodsId', verifyToken, isAdmin, updateGoods);
+router.patch('/admin/goods/:goodsId', authMiddleware, requireAdmin, updateGoods);
 
 /**상품 목록 조회  API router**/
 router.get('/goods', getGoods);
+
 export default router;
