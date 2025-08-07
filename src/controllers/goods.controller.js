3f2176f1f854771ae2fdfd45212d29bf8482cc8e
Youjin980225 <top04022@gmail.com>
2025-08-07 11:37:34 +0900

    ♻️ refactor goods.controller 코드 리팩토링 진행
    - response를 위해 코드 리팩토링을 진행하였습니다.
    - 서버 오류를 error-handling middleware를 통해서 받도록 리팩토링 하였습니다.

--- src/controllers/goods.controller.js
+++ src/controllers/goods.controller.js
@@ -5,7 +5,7 @@ import { Prisma as GamePrisma } from '../../prisma/Game/generated/prisma/index.j
  * { name, cashAmount }
  * 출력 : 201(성공)/400(입력오류)/409(중복))/500(서버오류)
  * **/
-export const registGoods = async (req, res) => {
+export const registGoods = async (req, res, next) => {
   const { name, cashAmount } = req.body;
 
   //1.1 오류 표기 : name과 cashAmount입력되지 않을 경우 400반환
@@ -21,30 +21,26 @@ export const registGoods = async (req, res) => {
       },
     });
 
-    //1.3 클라이언트 응답 형식
-    const response = {
+    //1.3 성공응답
+    return res.status(201).json({
       id: newGoods.goodsId,
       name: newGoods.name,
       cashAmount: newGoods.cashAmount.toString(), //문자열로 cashAmount반환
-    };
-    //1.4 성공응답
-    return res.status(201).json(response);
+    });
   } catch (error) {
-    console.error('상품 등록 오류:', error);
-
-    //1.5 고유 제약 조건 위반시 오류 메세지
+    //1.4 고유 제약 조건 위반시 오류 메세지
     if (error.code === 'P2002') {
       return res.status(409).json({ message: '상품명이 존재합니다.' });
     }
-    //1.6그 외 서버 오류
-    return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    //1.5그 외 서버 오류
+    next(err);
   }
 };
 
 /**상품 삭제 API controller
  * 출력 : 200(성공)/400(입력오류)/404(데이터 없음)/500(서버오류)
  * **/
-export const deleteGoods = async (req, res) => {
+export const deleteGoods = async (req, res, next) => {
   const goodsId = parseInt(req.params.goodsId, 10);
 
   //2.1 오류 표기 : goodsId가 숫자가 아닌 경우 400반환
@@ -63,16 +59,22 @@ export const deleteGoods = async (req, res) => {
     }
 
     //2.4 상품 삭제
-    await gamePrisma.goods.delete({
-      where: { goodsId },
+    const deleted = await gamePrisma.goods.delete({
+      where: { goodsId: parseInt(goodsId) },
     });
 
     //2.5 성공응답
-    res.status(200).json({ message: '상품이 삭제되었습니다.' });
+    res.status(200).json({
+      message: '상품이 삭제되었습니다.',
+      deleted: {
+        id: deleted.goodsId,
+        name: deleted.name,
+        cashAmount: deleted.cashAmount.toString(),
+      },
+    });
   } catch (error) {
     //2.6 그 외 서버 오류
-    console.error('상품 삭제 오류:', error);
-    return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    next(err);
   }
 };
 
@@ -80,7 +82,7 @@ export const deleteGoods = async (req, res) => {
  * { name, cashAmount }
  * 출력 : 200(성공)/400(입력오류)/404(데이터 없음))/500(서버오류)
  * **/
-export const updateGoods = async (req, res) => {
+export const updateGoods = async (req, res, next) => {
   const goodsId = parseInt(req.params.goodsId, 10);
   const { name, cashAmount } = req.body;
 
@@ -103,8 +105,8 @@ export const updateGoods = async (req, res) => {
     }
 
     //3.4 상품 정보 업데이트(입력된 필드만 변경)
-    const updateGoods = await gamePrisma.goods.update({
-      where: { goodsId },
+    const updated = await gamePrisma.goods.update({
+      where: { goodsId: parseInt(goodsId) },
       data: {
         ...(name && { name }), //name이 있을 경우 업데이트
         ...(cashAmount && { cashAmount }), //cashAmount가 있을 경우 업데이트
@@ -114,53 +116,44 @@ export const updateGoods = async (req, res) => {
     //3.5 성공 응답
     return res.status(200).json({
       message: '상품 수정이 완료되었습니다.',
-      name: updateGoods.name,
-      cashAmount: updateGoods.cashAmount,
+      updateGoods: {
+        id: updated.goodsId,
+        name: updated.name,
+        cashAmount: updated.cashAmount.toString(),
+      },
     });
   } catch (error) {
-    console.error('상품 수정 오류:', error);
-
     //3.6 고유 제약 조건 위반 시 409 반환
     if (error.code === 'P2002') {
       return res.status(409).json({ message: '상품명이 존재합니다.' });
     }
 
     //3.7 그 외 서버 오류
-    return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    next(error);
   }
 };
 
 /**상품 목록 조회 API
  * * 출력 : 200(성공)/404(데이터 없음)/500(서버오류)
  * **/
-export const getGoods = async (req, res) => {
+export const getGoods = async (req, res, next) => {
   try {
     //4.1 상품 목록 조회
-    const goodsList = await gamePrisma.goods.findMany({
-      select: {
-        goodsId: true,
-        name: true,
-        cashAmount: true,
-      },
-    });
+    const goodsList = await gamePrisma.goods.findMany();
 
     //4.2 조회 결과가 없을 경우 404반환
     if (goodsList.length === 0) {
       return res.status(404).json({ message: '등록된 상품이 없습니다.' });
     }
 
-    //4.3 클라이언트 응답 형식
-    const response = goodsList.map((goodsItem) => ({
-      id: goodsItem.goodsId,
-      name: goodsItem.name,
-      cashAmount: goodsItem.cashAmount.toString(), //문자열로 반환
+    //4.3 성공 응답
+    const formattedGoodsList = goodsList.map((goods) => ({
+      id: goods.goodsId,
+      name: goods.name,
+      cashAmount: goods.cashAmount.toString(),
     }));
-
-    //4.4 성공 응답
-    return res.status(200).json(response);
+    return res.status(200).json(formattedGoodsList);
   } catch (error) {
-    //4.5 그 외 서버 오류
-    console.error('상품 목록 조회 오류', error);
-    return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    next(err);
   }
 };
