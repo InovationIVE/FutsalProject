46af90cc16341c7a8f0c70ac10708e37e12b4bd0
Youjin980225 <top04022@gmail.com>
2025-08-07 14:52:29 +0900

    🔥 delete authMiddleware 삭제

--- src/routes/goods.router.js
+++ src/routes/goods.router.js
@@ -6,18 +6,18 @@ import {
   updateGoods,
   getGoods,
 } from '../controllers/goods.controller.js';
-import { authMiddleware, requireAdmin } from './auth.router.js';
+import { requireAdmin } from './auth.router.js';
 
 const router = express.Router();
 
 /**상품 등록 API router(관리자)**/
-router.post('/admin/goods', authMiddleware, requireAdmin, registGoods);
+router.post('/admin/goods', requireAdmin, registGoods);
 
 /**상품 삭제 API router(관리자)**/
-router.delete('/admin/goods/:goodsId', authMiddleware, requireAdmin, deleteGoods);
+router.delete('/admin/goods/:goodsId', requireAdmin, deleteGoods);
 
 /**상품 수정 API router(관리자)**/
-router.patch('/admin/goods/:goodsId', authMiddleware, requireAdmin, updateGoods);
+router.patch('/admin/goods/:goodsId', requireAdmin, updateGoods);
 
 /**상품 목록 조회  API router**/
 router.get('/goods', getGoods);
