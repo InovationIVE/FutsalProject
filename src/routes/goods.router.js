186c8d284f3fa41f835d0da84fa1ce35b61b8d92
Youjin980225 <top04022@gmail.com>
2025-08-07 15:53:00 +0900

    ♻️ refactor 코드 리팩토링
    - class 함수를 이용한 코드리팩토링

--- src/routes/goods.router.js
+++ src/routes/goods.router.js
@@ -1,25 +1,20 @@
 import express from 'express';
 import { gamePrisma } from '../utils/prisma/index.js';
-import {
-  registGoods,
-  deleteGoods,
-  updateGoods,
-  getGoods,
-} from '../controllers/goods.controller.js';
+import { GoodsController } from '../controllers/goods.controller.js';
 import { requireAdmin } from './auth.router.js';
 
 const router = express.Router();
 
 /**상품 등록 API router(관리자)**/
-router.post('/admin/goods', requireAdmin, registGoods);
+router.post('/admin/goods', requireAdmin, GoodsController.registGoods);
 
 /**상품 삭제 API router(관리자)**/
-router.delete('/admin/goods/:goodsId', requireAdmin, deleteGoods);
+router.delete('/admin/goods/:goodsId', requireAdmin, GoodsController.deleteGoods);
 
 /**상품 수정 API router(관리자)**/
-router.patch('/admin/goods/:goodsId', requireAdmin, updateGoods);
+router.patch('/admin/goods/:goodsId', requireAdmin, GoodsController.updateGoods);
 
 /**상품 목록 조회  API router**/
-router.get('/goods', getGoods);
+router.get('/goods', GoodsController.getGoods);
 
 export default router;
