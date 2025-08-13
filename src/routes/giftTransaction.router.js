ae95454f5186db211080dc8d4b2ebc5c0b75c741
Youjin980225 <top04022@gmail.com>
2025-08-13 09:35:09 +0900

    🎉 html, css 디자인 수정

--- src/routes/giftTransaction.router.js
+++ src/routes/giftTransaction.router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import giftTransactionController from '../controllers/giftTransaction.controller.js';
-import { requireAdmin } from '../middleWares/auth.middleware.js';
+import { authMiddleware, requireAdmin } from '../middleWares/auth.middleware.js';
 
 const router = express.Router();
 
@@ -11,7 +11,12 @@ router.post('/gift/send', giftTransactionController.sendGift);
 router.get('/gift/received/:receiverId', giftTransactionController.getReceivedGifts);
 
 /**전체 선물 내역 조회(관리자) API router**/
-router.get('/admin/gift', requireAdmin, giftTransactionController.getAllGiftTransactions);
+router.get(
+  '/admin/gift',
+  authMiddleware,
+  requireAdmin,
+  giftTransactionController.getAllGiftTransactions,
+);
 
 /**선물받기 API router**/
 router.post('/gift/accept/:giftId', giftTransactionController.acceptGift);
--- src/routes/goods.router.js
+++ src/routes/goods.router.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import { gamePrisma } from '../utils/prisma/index.js';
 import { GoodsController } from '../controllers/goods.controller.js';
-import { requireAdmin } from '../middleWares/auth.middleware.js';
+import { authMiddleware, requireAdmin } from '../middleWares/auth.middleware.js';
 
 const router = express.Router();
 
 /**상품 등록 API router(관리자)**/
-router.post('/admin/goods', requireAdmin, GoodsController.RegistGoods);
+router.post('/admin/goods', authMiddleware, requireAdmin, GoodsController.RegistGoods);
 
 /**상품 삭제 API router(관리자)**/
-router.delete('/admin/goods/:goodsId', requireAdmin, GoodsController.DeleteGoods);
+router.delete('/admin/goods/:goodsId', authMiddleware, requireAdmin, GoodsController.DeleteGoods);
 
 /**상품 수정 API router(관리자)**/
-router.patch('/admin/goods/:goodsId', requireAdmin, GoodsController.UpdateGoods);
+router.patch('/admin/goods/:goodsId', authMiddleware, requireAdmin, GoodsController.UpdateGoods);
 
 /**상품 목록 조회  API router**/
 router.get('/goods', GoodsController.GetGoods);
