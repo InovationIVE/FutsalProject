2982126b29643d439d5e727be6831c07ee767612
gguti <ggutips94@gmail.com>
2025-08-14 16:27:09 +0900

    🐛 fix count를 모델필드에서 없애서 관련 과정 제거

--- src/controllers/ownedPlayers.controller.js
+++ src/controllers/ownedPlayers.controller.js
@@ -16,12 +16,14 @@ export class OwnedPlayersController {
         where: { accountId: +accountId },
         select: {
           ownedPlayerId: true,
+          playerId: true,
           name: true,
           level: true,
           rarity: true,
           attack: true,
           defence: true,
           speed: true,
+          level: true,
         },
       });
 
@@ -104,75 +106,58 @@ export class OwnedPlayersController {
       });
 
       // 3. 응답 조합
-      return res.status(200).json({message:`${ownedPlayer.name}의 세부정보입니다.`, ...ownedPlayer, profileImage: player?.profileImage || 'null' });
+      return res.status(200).json({
+        message: `${ownedPlayer.name}의 세부정보입니다.`,
+        ...ownedPlayer,
+        profileImage: player?.profileImage || 'null',
+      });
     } catch (err) {
       next(err);
     }
   }
-
   //보유 선수 판매
   static async playerSale(req, res, next) {
     try {
       const { ownedPlayerId } = req.body;
       const { accountId } = req.user;
-
       if (!ownedPlayerId) {
         return res.status(400).json({ message: '유효한 ownedPlayerId를 입력해주세요.' });
       }
-
       const ownedPlayer = await userPrisma.ownedPlayers.findFirst({
         where: {
           ownedPlayerId: +ownedPlayerId,
           accountId: +accountId,
         },
       });
-
       if (!ownedPlayer) {
         return res.status(404).json({ message: '해당 선수를 찾을 수 없습니다.' });
       }
-
-      // 카운트 삭제로 인한 주석처리
-      // if (ownedPlayer.count < count) {
-      //   return res.status(400).json({ message: '보유 수량보다 많이 판매할 수 없습니다.' });
-      // }
-
       /** 레어도에 따른 가격 책정 **/
       const ownedPlayerInfo = await PlayerModel.getSome(ownedPlayer.playerId);
       const own_rarity_price = await gamePrisma.rarityPrice.findUnique({
-         where:{ rarity : ownedPlayerInfo.rarity}
+        where: { rarity: ownedPlayerInfo.rarity },
       });
-
       /** 레어도 비용 에 (보유선수레벨*10)% 만큼 가산 후, 100의 자리수에서 버림**/
-      const gain = Math.floor( (own_rarity_price.priceForRarity * (ownedPlayer.level/10 + 1)) /100 )*100; 
-
+      const gain =
+        Math.floor((own_rarity_price.priceForRarity * (ownedPlayer.level / 10 + 1)) / 100) * 100;
       await userPrisma.$transaction(
         async (tx) => {
           await tx.account.update({
             where: { accountId: +accountId },
             data: { cash: { increment: gain } },
           });
-
-          if (ownedPlayer.count > count) {
-            await tx.ownedPlayers.update({
-              where: { ownedPlayerId: ownedPlayer.ownedPlayerId },
-              data: { count: { decrement: count } },
-            });
-          } else {
-            await tx.ownedPlayers.delete({
-              where: { ownedPlayerId: ownedPlayer.ownedPlayerId },
-            });
-          }
+          await tx.ownedPlayers.delete({
+            where: { ownedPlayerId: ownedPlayer.ownedPlayerId },
+          });
         },
         {
           isolationLevel: UserPrisma.TransactionIsolationLevel.ReadCommitted,
         },
       );
-
       const updated = await userPrisma.account.findUnique({
         where: { accountId: +accountId },
         select: { cash: true },
       });
-
       return res.status(200).json({
         message: '선수판매 완료',
         sales: gain,
